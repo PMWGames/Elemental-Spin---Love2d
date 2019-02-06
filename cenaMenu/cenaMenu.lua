dfb51b99d77f0833030653bb6bdd583203378ab6
Rafael Araújo <rafaelskate7@gmail.com>
2019-02-05 22:38:25 -0300

    +

--- cenaMenu/cenaMenu.lua
+++ cenaMenu/cenaMenu.lua
@@ -1,7 +1,9 @@
-
+require "libs/flame"
 
 function cenaMenu_load()
+
     fundo = love.graphics.newImage("cenaMenu/backgroud.png")
+
 end
 
 function cenaMenu_update(dt)
@@ -9,5 +11,9 @@ function cenaMenu_update(dt)
 end
 
 function cenaMenu_draw()
-    love.graphics.draw(fundo, 0, 0)
+
+
+    flame_drawImageScreenPercent(fundo, 101, 0, -1)
+   
+
 end
--- libs/flame.lua
+++ libs/flame.lua
@@ -1,13 +1,16 @@
 
 
-function flame_drawImageScreenPercent(image, percent, pos_x, pos_y)
+function flame_drawImageScreenPercent(image, percent, pos_x_percent, pos_y_percent)
 
     local tam_x = love.graphics.getWidth();
     local tam_y = love.graphics.getHeight();
 
+    pos_x_per = (tam_x * pos_x_percent) / 100 / 2;
+    pos_y_per = (tam_y * pos_y_percent) / 100 / 2;
+
     tam_x = ((tam_x / image:getWidth()) * percent) / 100;
     tam_y = ((tam_y / image:getHeight()) * percent) / 100;
 
-    love.graphics.draw(image, pos_x, pos_y, 0, tam_x, tam_y);
+    love.graphics.draw(image, pos_x_per, pos_y_per, 0, tam_x, tam_y);
 
 end
