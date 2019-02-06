49c4294b5ab244d70fd694c82701c931ce597b3c
PazJulia <paz.julia7@gmail.com>
2019-02-05 21:16:03 -0300

    fundo

---
+++ cenaMenu/cenaMenu.lua
@@ -0,0 +1,12 @@
+
+function love.load()
+    fundo = love.graphics.newImage("background.png")
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.draw(fundo, 100, 100)
+end
